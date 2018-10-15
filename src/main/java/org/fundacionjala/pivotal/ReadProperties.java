5fe11b9464984ca19fdceb966e24d71f1eeddb60
Kevin <kevin.sanchezpeep@gmail.com>
2018-10-15 00:29:15 -0700

    add doc

--- src/main/java/org/fundacionjala/pivotal/ReadProperties.java
+++ src/main/java/org/fundacionjala/pivotal/ReadProperties.java
@@ -5,9 +5,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * Class which reads properties file.
+ * @author Kevin Sanchez AWT - [01].
+ * @version 0.1
+ */
 public class ReadProperties {
     private Properties properties;
     private InputStream input;
+
+    /**
+     * Constructor which reads the file.
+     */
     private ReadProperties() {
         properties = new Properties();
         input = null;
@@ -19,10 +28,18 @@ public class ReadProperties {
         }
     }
 
+    /**
+     * getter of properties.
+     * @return properties
+     */
     public Properties getProperties() {
         return properties;
     }
 
+    /**
+     * return the browser according the properties file.
+     * @return string
+     */
     public static String getBrowser() {
         ReadProperties readProperties = new ReadProperties();
         return readProperties.getProperties().getProperty("browser");
