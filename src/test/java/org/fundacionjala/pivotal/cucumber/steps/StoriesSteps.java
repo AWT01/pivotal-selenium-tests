4517994b3e876bf75f71165fd24c1c7c61f26c0f
FUNDACIONJALA\KevinSanchez <kevin.sanchezpeep@gmail.com>
2018-10-18 14:45:42 -0400

    improve stories

--- src/test/java/org/fundacionjala/pivotal/cucumber/steps/StoriesSteps.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/steps/StoriesSteps.java
@@ -1,18 +1,9 @@
 package org.fundacionjala.pivotal.cucumber.steps;
 
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.fundacionjala.core.ui.WebDriverManager;
-import org.fundacionjala.core.util.Environment;
-import org.fundacionjala.pivotal.pageobjects.dashboard.Dashboard;
-import org.fundacionjala.pivotal.pageobjects.login.SignInPage;
 import org.fundacionjala.pivotal.pageobjects.story.Story;
 import org.testng.Assert;
-
-import java.util.Map;
 
 /**
  * Class with the steps to create a story.
@@ -20,47 +11,23 @@ import java.util.Map;
  * @version 0.1
  */
 public class StoriesSteps {
-    private static final Logger LOGGER = LogManager.getLogger("Read properties");
     private Story storyPage;
-    /**
-     * Precondition, user must be logged in
-     * and enter in a existing project.
-     *
-     * @param username inserted
-     * @param password inserted
-     */
-    @Given("^username \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void usernameAndPassword(final String username, final String password) {
-        Dashboard dashboard;
-        try {
-            dashboard = SignInPage.newCredentials(Environment.getInstance().getProperties()
-                    .getProperty(username), Environment.getInstance().getProperties()
-                    .getProperty(password));
-        } catch (NullPointerException exception) {
-            LOGGER.error("The parameters of username or password are missing in the config.properties file");
-            throw exception;
-        }
-
-        WebDriverManager.getInstance().getDriver()
-                .get(dashboard.getFirstProject());
-        storyPage = new Story();
-    }
-
     /**
      * click add story action.
      */
-    @Then("^I click the add story button$")
+    @And("^I click the add story button$")
     public void iClickAddStoryButton() {
+        storyPage = new Story();
         storyPage.clickAddSideBarStoryButton();
     }
 
     /**
      * file the title of the story with the data table.
-     * @param value map for data table
+     * @param title of the story
      */
-    @And("^I fill the story title$")
-    public void iFillStoryTitle(final Map<String, String> value) {
-        storyPage.setStoryTitle(value.get("title"));
+    @And("^I fill the story \"([^\"]*)\"$")
+    public void iFillStoryTitle(final String title) {
+        storyPage.setStoryTitle(title);
     }
 
     /**
@@ -69,6 +36,6 @@ public class StoriesSteps {
     @Then("^I verify if the story was created$")
     public void iVerifyIfStoryWasCreated() {
         storyPage.clickSaveButton();
-        Assert.assertEquals(storyPage.getFirstStory(), "testStory");
+        Assert.assertEquals(storyPage.getFirstStory(), "storyTest");
     }
 }
--- src/test/resources/features/stories.feature
+++ src/test/resources/features/stories.feature
@@ -1,7 +1,12 @@
 Feature: Manage Stories
   Scenario: Create a new story
-    Given username "user1" and password "password1"
-    Then I click the add story button
-    And I fill the story title
-    |title|testStory|
+    Given I log in as "adrinrojas1" "test12345"
+    When I click the create project button
+    And I fill the fields of Create new project and press the create button
+      | name    | Public project |
+      | account | newAccount     |
+      | privacy | public         |
+
+    And I click the add story button
+    And I fill the story "storyTest"
     Then I verify if the story was created
